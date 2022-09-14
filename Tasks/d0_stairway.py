6931612eb235d6312c950c06a00ce7b07fb96ab6
kirfed <dquodwypo@yandex.ru>
2022-09-14 20:29:54 +0300

    14.09.2022 Practice

--- Tasks/d0_stairway.py
+++ Tasks/d0_stairway.py
@@ -1,4 +1,13 @@
+import timeit
 from typing import Union, Sequence
+
+
+def stairway_path_rec(cost: Sequence[Union[float, int]], i: int, fin: int):
+    cost[i] += min(cost[i - 1], cost[i - 2])
+    if i == fin:
+        return cost[i]
+    else:
+        return stairway_path_rec(cost, i + 1, fin)
 
 
 def stairway_path(stairway: Sequence[Union[float, int]]) -> Union[float, int]:
@@ -8,5 +17,19 @@ def stairway_path(stairway: Sequence[Union[float, int]]) -> Union[float, int]:
     :param stairway: list of ints, where each int is a cost of appropriate step
     :return: minimal cost of getting to the top
     """
-    print(stairway)
-    return 0
+    size = len(stairway)
+    return stairway_path_rec(stairway, 2, size - 1)
+
+
+def stairway_path2(stairway: Sequence[Union[float, int]]) -> Union[float, int]:
+    """
+    Calculate min cost of getting to the top of stairway if agent can go on next or through one step.
+
+    :param stairway: list of ints, where each int is a cost of appropriate step
+    :return: minimal cost of getting to the top
+    """
+    size = len(stairway)
+    cost = stairway[:]
+    for i in range(2, size):
+        cost[i] += min(cost[i-1], cost[i-2])
+    return cost[-1]
--- Tasks/d1_horse.py
+++ Tasks/d1_horse.py
@@ -6,5 +6,31 @@ def calculate_paths(shape: (int, int), point: (int, int)) -> int:
     :param point: desired point for horse
     :return: count of paths from (1, 1) to (point[0], point[1]) (numerating from 0, so (0, 0) - left bottom tile)
     """
-    print(shape, point)
-    return 0
+    rows = shape[0]
+    cols = shape[1]
+    desk = [[0] * (rows + 4) for _ in range((cols + 4))]
+    p = [point[0] + 2, point[1] + 2]
+    desk[3][3] = 1
+    while 1:
+        cnt = 0
+        for i in range(2, rows - 2):
+            for j in range(2, cols - 2):
+                if desk[i][j] != 0 and i != p[0] and j != p[1]:
+                    cnt += 1
+                    desk[i - 2][j - 1] += desk[i][j]
+                    desk[i - 2][j + 1] += desk[i][j]
+                    desk[i + 2][j - 1] += desk[i][j]
+                    desk[i + 2][j + 1] += desk[i][j]
+                    desk[i - 1][j - 2] += desk[i][j]
+                    desk[i - 1][j + 2] += desk[i][j]
+                    desk[i + 1][j - 2] += desk[i][j]
+                    desk[i + 1][j + 2] += desk[i][j]
+                    # не закончено
+        if cnt == 0:
+            break
+    #for j in range(cols + 4):
+    #    print(desk[j])
+    return desk[point[0]][point[1]]
+
+
+calculate_paths((4, 4), (3, 3))
